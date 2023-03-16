27d0232586664a5f16bf3a191bb1a2bccdcaadc7
NoeDelCorti <delcortinoelia@gmail.com>
2023-03-16 12:18:57 -0300

    creados los filtros. error en repeticion de eventos

--- assets/JS/upcoming_events.js
+++ assets/JS/upcoming_events.js
@@ -1,6 +1,39 @@
-console.log([document]);
-const divElementos = document.getElementById('elementos');
+const divElementos = document.getElementById('elementos')
+const divCheckFilter = document.getElementById('checkFilter')
+const input = document.querySelector('input')
+
+input.addEventListener('input',filtroDoble)
+divCheckFilter.addEventListener('change',filtroDoble)
+
+function filtroDoble(){
+    let primerFiltro = filtrarPorTexto(data.events,input.value)
+    let segundoFiltro = filtrarPorCategoria(primerFiltro)
+    pintarEventos(segundoFiltro)
+}
+
+pintarEventos(data.events)
+crearCheckBoxes(data.events)
+
+function crearCheckBoxes(array){
+    let arrayCategorias = array.map(evento => evento.category)
+    let setCategoria = new Set(arrayCategorias)
+    let arrayChecks = Array.from(setCategoria)
+    let checkboxes = ''
+    arrayChecks.forEach(category => {
+        checkboxes += `<div class="form-check form-switch">
+        <input class="form-check-input" type="checkbox" role="switch" id="${category}" value="${category}">
+        <label class="form-check-label" for="${category}">${category}</label>
+      </div>`
+    })
+    divCheckFilter.innerHTML = checkboxes
+}
+function pintarEventos(array){
+    if(array.length == 0){
+        divElementos.innerHTML = `<h2 class="display-1 fw-bolder">Not events</h2>`
+        return
+    }
 let tarjetas = ' '
+array.forEach(evento => {
 const currentDate = new Date(data.currentDate);
  console.log(currentDate);
  for (let i = 0; i < data.events.length; i++) {
@@ -18,7 +51,28 @@ const currentDate = new Date(data.currentDate);
                 </div>
             </div>
             </div>`
-            }
+            } }
+        })
             console.log(tarjetas);
             divElementos.innerHTML = tarjetas
+    }
+
+    function filtrarPorTexto(array,texto){
+        let arrayFiltrado = array.filter(elemento => elemento.name.toLowerCase().includes(texto.toLowerCase()))
+        return arrayFiltrado
+    }
+    function filtrarPorCategoria(array){
+        let checkboxes = document.querySelectorAll("input[type='checkbox']")
+        console.log(checkboxes);
+        let arrayChecks = Array.from(checkboxes)
+        let arrayChecksChecked = arrayChecks.filter(check => check.checked)
+        console.log(arrayChecksChecked);
+        let arrayChecksCheckedValues = arrayChecksChecked.map(checkChecked => checkChecked.value)
+        console.log(arrayChecksCheckedValues);
+        let arrayFiltrado = array.filter(elemento => arrayChecksCheckedValues.includes(elemento.category))
+        console.log(arrayFiltrado);
+        if(arrayChecksChecked.length > 0){
+            return arrayFiltrado
+        }
+        return array
     }
